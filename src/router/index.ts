a410548fb78365f501d17be599fd933e57f8420d
Donald Gjeloshi <dongje11@htl-shkoder.com>
2019-11-27 15:00:26 +0100

    restore view /record

--- src/router/index.ts
+++ src/router/index.ts
@@ -18,11 +18,20 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/record",
+    name: "record",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "record" */ "../views/Record.vue")
   }
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  // mode: "history",
   base: process.env.BASE_URL,
   routes
 });
--- src/views/Record.vue
+++ src/views/Record.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="record">
-    <h1>record mouse movements</h1>
+  <div>
+    <div class="record">
+      <h1>record mouse movements</h1>
+      <h1>hallo world</h1>
+    </div>
+    <div class="square"></div>
   </div>
 </template>
+
+<style lang="stylus">
+.square {
+  width: 50%;
+  border-style: solid;
+  border-color: coral;
+}
+
+.square:after {
+  content: '';
+  display: block;
+  padding-bottom: 100%;
+}
+</style>
