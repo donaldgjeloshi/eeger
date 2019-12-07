3fb99b31ae72d4560d79917f43255b6d59dac415
Donald Gjeloshi <dongje11@htl-shkoder.com>
2019-12-07 17:31:35 +0100

    add a new component DrawingBoard

---
+++ src/components/KeyboardKeys.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div
+      class="col-sm-9"
+      v-focus
+      tabindex="0"
+      @keyup="squack('a button was clicked')"
+    >
+      <p>Press left or right button to get squack!</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    squack: function(text) {
+      alert(text);
+    }
+  },
+  directives: {
+    focus: {
+      inserted(el) {
+        el.focus();
+      }
+    }
+  }
+};
+</script>
+<style lang="stylus" scoped>
+div {
+  margin-top: 20px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <drawing-board></drawing-board>
+    <keyboard-keys></keyboard-keys>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import DrawingBoard from "@/components/DrawingBoard";
+import KeyboardKeys from "@/components/KeyboardKeys";
 
 export default {
   name: "home",
   components: {
-    DrawingBoard
+    DrawingBoard,
+    KeyboardKeys
   }
 };
 </script>
