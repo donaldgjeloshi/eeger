b534aab951c2bab1c8283cf0858636214cb6edf6
Donald Gjeloshi <dongje11@htl-shkoder.com>
2019-12-10 17:12:41 +0100

    show the typed key on the screen

--- src/components/KeyboardKeys.vue
+++ src/components/KeyboardKeys.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <input type="text" />
+    <div class="col-sm-9">
+      <canvas
+        v-on:keydown="type($event)"
+        ref="canvas"
+        width="100px"
+        height="70px"
+      ></canvas>
+    </div>
   </div>
 </template>
 <script>
@@ -9,8 +16,17 @@ export default {
     return {};
   },
   methods: {
-    typedKey: function() {
-      alert("hey there");
+    typedKey: function(event) {
+      const c = this.$refs.canvas;
+      const ctx = c.getContext("2d");
+      ctx.font = "30px Arial";
+      //clear the canvas before typing the new number
+      ctx.clearRect(0, 0, c.width, c.height);
+      ctx.beginPath(); //see mdn docu
+      //convert Unicode value in characters
+      const char = String.fromCharCode(event.keyCode);
+      //show the number
+      ctx.fillText(char, 40, 50);
     }
   },
   mounted() {
@@ -18,4 +34,10 @@ export default {
   }
 };
 </script>
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+canvas {
+  background: white;
+  box-shadow: 0px 2px 3px rgba(0, 0, 0, 0.2);
+  margin-top: 20px;
+}
+</style>
